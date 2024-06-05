3c1a4716a620317a4ba6ff89c0efec2d4bf2b354
lunghr <myilanka8@gmail.com>
2024-06-05 14:06:51 +0300

    Initial commit

---
+++ src/main/java/com/lunghr/count_math_lab3/CountMathLab3Application.java
@@ -0,0 +1,13 @@
+package com.lunghr.count_math_lab3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountMathLab3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountMathLab3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/lunghr/count_math_lab3/CountMathLab3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lunghr.count_math_lab3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountMathLab3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
